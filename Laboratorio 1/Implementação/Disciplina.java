95b3cca95330034571c656386b0bf9b1e6256a97
Richbert Oliveira <richbertstephano@gmail.com>
2022-08-17 19:55:23 -0300

    Esqueleto criado

---
+++ Laboratorio 1/Implementação/Disciplina.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Disciplina {
+
+    private ArrayList<Aluno> alunos;
+
+    public void validarNumALunos() {
+        if() {
+            this.alunos.size();
+        }
+    }
+}
