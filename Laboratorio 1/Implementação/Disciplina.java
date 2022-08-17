708a04a82d90e71cd7666172e0402179d673b76f
Richbert Oliveira <richbertstephano@gmail.com>
2022-08-17 20:40:39 -0300

    Update em caso de uso e criação de mais esqueletos

--- Laboratorio 1/Implementação/Disciplina.java
+++ Laboratorio 1/Implementação/Disciplina.java
@@ -1,12 +1,66 @@
 import java.util.ArrayList;
 
 public class Disciplina {
+    private static final int MAX_ALUNOS = 60;
+    private static final int MIN_ALUNOS = 3;
 
+    private String nome;
+    private boolean estaAtiva;
+    private boolean obrigatoria;
     private ArrayList<Aluno> alunos;
 
-    public void validarNumALunos() {
-        if() {
-            this.alunos.size();
+    public Disciplina() {
+        this.nome = "";
+        this.estaAtiva = true;
+        this.obrigatoria = false;
+        this.alunos = new ArrayList<Aluno>();
+    }
+
+    public Disciplina(String nome, boolean estaAtiva, boolean obrigatoria) {
+        this.nome = nome;
+        this.estaAtiva = estaAtiva;
+        this.obrigatoria = obrigatoria;
+        this.alunos = new ArrayList<Aluno>();
+    }
+
+    public Disciplina(String nome, boolean estaAtiva, boolean obrigatoria, ArrayList<Aluno> alunos) {
+        this.nome = nome;
+        this.estaAtiva = estaAtiva;
+        this.obrigatoria = obrigatoria;
+        this.alunos = alunos;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean getObrigatoria() {
+        return this.obrigatoria;
+    }
+    
+    public void setObrigatoria(boolean obrigatoria) {
+        this.obrigatoria = obrigatoria;
+    }
+
+    public boolean getEstaAtiva() {
+        return this.estaAtiva;
+    }
+
+    public void setEstaAtiva(boolean estaAtiva) {
+        this.estaAtiva = estaAtiva;
+    }
+
+    public boolean validarNumALunos() {
+        int tamAlunos = this.alunos.size();
+        
+        if(tamAlunos == MAX_ALUNOS) {
+            return false;
         }
+        
+        return true;
     }
 }
--- Laboratorio 1/Implementação/Matricula.java
+++ Laboratorio 1/Implementação/Matricula.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Matricula {
-    
+    private Date dataInicio;
+    private Date dataFim;
+    private Aluno aluno;
+    private ArrayList<Disciplina> disciplinas;
+    private double mensalidade;
+
+    public Matricula() {
+        this.nome = "";
+        this.estaAtiva = true;
+        this.obrigatoria = false;
+        this.alunos = new ArrayList<Aluno>();
+    }
+
+    public Matricula(String nome, boolean estaAtiva, boolean obrigatoria) {
+        this.nome = nome;
+        this.estaAtiva = estaAtiva;
+        this.obrigatoria = obrigatoria;
+        this.alunos = new ArrayList<Aluno>();
+    }
+
+    public Matricula(String nome, boolean estaAtiva, boolean obrigatoria, ArrayList<Aluno> alunos) {
+        this.nome = nome;
+        this.estaAtiva = estaAtiva;
+        this.obrigatoria = obrigatoria;
+        this.alunos = alunos;
+    }
 }
