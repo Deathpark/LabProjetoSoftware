101677679b1b365e6aa5db8a319a4067ecc1e35c
Richbert Oliveira <richbertstephano@gmail.com>
2022-08-24 20:58:28 -0300

    Alterações em diagramas e alterações em secretaria

--- Laboratorio 1/Implementação/Secretaria.java
+++ Laboratorio 1/Implementação/Secretaria.java
@@ -30,11 +30,20 @@ public class Secretaria extends Usuario {
     }
 
     public void excluirDisciplina(Disciplina disciplina) {
-    
+        boolean removeu = this.disciplinas.removeIf(d -> d.equals(disciplina));
+
+        if(removeu) {
+            System.out.println("Disciplina removida com sucesso!");
+        } else {
+            System.out.println("Disciplina não encontrada!");
+        }
     }
 
     public void atualizarAluno(Aluno aluno){
-
+        int resposta = 1;
+        while(resposta > 0) {
+        }
+        Aluno alunoAux = this.alunos.stream().filter(a -> a.equals(aluno)).findFirst().get();    
     }
 
     public void criarAluno() {
@@ -42,7 +51,13 @@ public class Secretaria extends Usuario {
     }
 
     public void excluirAluno(Aluno aluno) {
+        boolean removeu = this.alunos.removeIf(a -> a.equals(aluno));
 
+        if(removeu) {
+            System.out.println("Aluno removido com sucesso!");
+        } else {
+            System.out.println("Aluno não encontrado!");
+        }
     }
 
     public void visualizarAluno(Aluno aluno) {
@@ -58,10 +73,30 @@ public class Secretaria extends Usuario {
     }
 
     public void excluirProfessor(Professor professor) {
+        boolean removeu = this.professores.removeIf(p -> p.equals(professor));
 
+        if(removeu) {
+            System.out.println("Professor removido com sucesso!");
+        } else {
+            System.out.println("Professor não encontrado!");
+        }
     }
 
     public void visualizarProfessor(Professor professor) {
         System.out.println(professor.toString());
+    }
+
+    public void menuAtualizacaoCadastros(String opcao) {
+        System.out.println("Qual característica do(a) " + opcao + " será modificada?");
+        System.out.println("1 - Nome do(a) " + opcao + ".");
+        System.out.println("2 - Senha do(a) " + opcao + ".");
+
+        if(opcao.equals("aluno(a)")) {
+            System.out.println("3 - Matricula atual do(a) " + opcao + ".");
+        } else if(opcao.equals("professor(a)")) {
+            System.out.println("3 - Disciplinas que o(a) " + opcao + " leciona.");
+        } else if(opcao.equals("disciplina")) {
+            System.out.println("3 - Nome da " + opcao);
+        }
     }
 }
