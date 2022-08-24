fce05c1f70cc17d667ce251dd2dbdffefea9b391
Sara Iglesias <sara.iglesias0309@gmail.com>
2022-08-24 19:52:22 -0300

    Construtor da classe Semestre

--- Laboratorio 1/Implementação/Semestre.java
+++ Laboratorio 1/Implementação/Semestre.java
@@ -4,5 +4,13 @@ public class Semestre {
     private ArrayList<Disciplina> disciplinas;
     private int codigo;
 
+    public Semestre (){
+        this.disciplinas = new ArrayList<Disciplina>();
+        this.codigo = 0;
+    }
+
+    public void ValidaCodigo(){
+        
+    }
     
 }
