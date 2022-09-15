1368bf89b2c7e0e80059758d69984505df752de2
Richbert Oliveira <richbertstephano@gmail.com>
2022-09-15 10:58:11 -0300

    Update em diagrama de pacotes

---
+++ Laboratorio 2/Implementação/LabDesSoftware02/src/main/java/LeticiaRichbertSara/LabDesSoftware02/Automovel.java
@@ -0,0 +1,60 @@
+package LeticiaRichbertSara.LabDesSoftware02;
+
+public class Automovel {
+    private int ano;
+    private String matricula;
+    private String marca;
+    private String modelo;
+    private String placa;
+    private int proprietario;
+
+
+    public int getAno() {
+        return this.ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String getMatricula() {
+        return this.matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return this.modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPlaca() {
+        return this.placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getProprietario() {
+        return this.proprietario;
+    }
+
+    public void setProprietario(int proprietario) {
+        this.proprietario = proprietario;
+    }
+
+}
